607205ec696dfbc9cdd49dbd8f74d31e20512842
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-01-26 11:54:05 +0100

    add 17 TODO: in world.class.js

--- models/world.class.js
+++ models/world.class.js
@@ -257,3 +257,4 @@ class World {
 // TODO: 16.3  If Conditions in eine separate return Funktion auslagern und den Namen als eine Frage geben, z.B. canMoveRight() {return .....}.
 // TODO: 16.4  Den Funktionsblock z.B der IfAbfrage canMoveRight() bei der Character Klasse in eine separate moveRight() auslagern. Wenn diese separate Fn auch eine Fn mit dem gleichen Namen ausführt, d.h. z.B. moveRight() führt eine moveRight() aus der vererbenden SuperKlasse movableObject.class.js aus, dann ist diese geerbte Fn, hier moveRight(), mittels super.moveRight() (zur Differeniezrung / "Kollision" bei gleicher FunktionsNamensGebung / Fehlervermeidung) innnerhalb der separaten Fn moveRight() auszuführen.
 // TODO: 16.5  Die IfAbfrage kann auch auf eine Zeile ohne {} Klammern verkürzt werden, wenn nur ein Statement im IfAbfrageBlock vorhanden ist.
+// TODO: 17.. FINACL CHECK / ES SOLL "PERFEKT": PORTFOLIO-READY SEIN!
