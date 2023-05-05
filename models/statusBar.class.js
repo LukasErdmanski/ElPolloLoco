d589759d826bbf81ef948be8615bc0a3ecb9b6b2
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-05-05 07:17:54 +0200

    For statusBar class: Cleaned; added JSDoc, tested

--- models/statusBar.class.js
+++ models/statusBar.class.js
@@ -1,27 +1,61 @@
+/**
+ * Represents a status bar.
+ * @extends DrawableObject
+ */
 class StatusBar extends DrawableObject {
-  // Storage of paths of status bar images.
+  /**
+   * The array of paths of status bar images.
+   * @type {string[]}
+   */
   IMAGES_PATHS_BAR = [];
-  // Current percentage of status bar.
+
+  /**
+   * The current percentage of the status bar.
+   * @type {number}
+   */
   percentage;
 
   /**
-   * Initializes the one of the possible status bars, character's health, bottles or coins amount and endboss' health.
-   * @param {number} x - The x coordinate of the bar.
-   * @param {number} y - The y coordinate of the bar.
-   * @param {[String]} imagePathsArr - The array of status bar image paths.
-   * @param {number} percentage - The initial percentage of the bar status to be presented.
+   * Creates a new StatusBar object.
+   * @param {number} x - The x coordinate of the status bar.
+   * @param {number} y - The y coordinate of the status bar.
+   * @param {string[]} imagePathsArr - The array of status bar image paths.
+   * @param {number} percentage - The initial percentage of the status bar to be presented.
    */
   constructor(x, y, imagePathsArr, percentage) {
-    // Call the constructor of the super class DrawbleObject.
     super();
-    this.x = x;
-    this.y = y;
-    this.width = 200;
-    this.height = 60;
-    this.IMAGES_PATHS_BAR = imagePathsArr;
-    // this.loadImageFromImageCache(this.IMAGES_PATHS_BAR[0]);
 
-    // Set initially the 'pecentage' property to the given pecentage.
+    /**
+     * The x coordinate of the status bar.
+     * @type {number}
+     */
+    this.x = x;
+
+    /**
+     * The y coordinate of the status bar.
+     * @type {number}
+     */
+    this.y = y;
+
+    /**
+     * The width of the status bar.
+     * @type {number}
+     */
+    this.width = 200;
+
+    /**
+     * The height of the status bar.
+     * @type {number}
+     */
+    this.height = 60;
+
+    /**
+     * The array of status bar image paths.
+     * @type {string[]}
+     */
+    this.IMAGES_PATHS_BAR = imagePathsArr;
+
+    // Set the initial percentage of the status bar.
     this.setPercentage(percentage);
   }
 
@@ -30,22 +64,28 @@ class StatusBar extends DrawableObject {
    * @param {number} percentage - The percentage of the status bar.
    */
   setPercentage(percentage) {
-      // Set the 'percentage' to the 'precentage' argument.
-      this.percentage = percentage;
-      // Set the local 'path' variable with the image file path from 'IMAGES' array with resolved index.
-      let path = this.IMAGES_PATHS_BAR[this.resolveImageIndex()];
-      // Set the 'img' property with the image object from 'imageCache' array with the key 'path'.
-      this.img = DrawableObject.imageCache[path];
+    /**
+     * The percentage of the status bar.
+     * @type {number}
+     */
+    this.percentage = percentage;
+
+    // Set the local 'path' variable with the image file path from 'IMAGES_PATHS_BAR' array with resolved index.
+    let path = this.IMAGES_PATHS_BAR[this.resolveImageIndex()];
+
+    // Set the 'img' property with the image object from 'imageCache' array with the key 'path'.
+    this.img = DrawableObject.imageCache[path];
   }
 
   /**
    * Returns the index of the status bar image that should be displayed.
-   * @returns {number} The index of the  status bar image to be displayed.
+   * @returns {number} The index of the status bar image to be displayed.
    */
   resolveImageIndex() {
     /**
      * Calculate and return only the integer part of the division 100 / 20, i.e. 5, 4, 3, 2, 1, 0, one of the possible
      * six indices 'IMAGES' array.
+     * @type {number}
      */
     return Math.ceil(this.percentage / 20);
   }
