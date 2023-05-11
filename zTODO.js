c98937a5b3cb464224b2ac4fa5d13c48b1c636a8
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-05-11 15:49:55 +0200

    Shadow uf button fixed dimensions of control und game info box on mobile fixed

--- zTODO.js
+++ zTODO.js
@@ -1,7 +1,5 @@
 // TODO: JSDoc final generieren
 
-// TODO: Shadow von Button flacker bei Handy
-
 // TODO: Fullscreen in Safari und Chrome auf Iphone
 
 // TODO: Stoppt Animation of Endboss when character is dead --> It shold be played till end
@@ -16,5 +14,3 @@
 
 // TODO: Jump Sound ab un zu 2mal --> hört sich elektronisch an ---> vielliecht doch allte uberpüfung von isPlaying Play Pause ind SoundClass machen
 // TODO: Vielleicht für Probleme mit Jump und MoveLeftRight Sound Howl für diese anders einstellen
-
-// TODO: Höhe von Control und Game Info box wenn Adress tool bar sichtbar muss auf max height von window eingestellt werden.
